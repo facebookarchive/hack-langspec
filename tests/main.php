771818af0d79fe7b269676d6aacd391ae9f01725
JoelMarcey <joelm@fb.com>
2015-02-12 15:55:04 -0800

    Revert "Add a main.php to require in the tests"
    This reverts commit 794baf592ec40f965d2d910915a20aa31ee0f3d1.

--- tests/main.php
+++
@@ -1,6 +0,0 @@
-<?hh
-
-// Require main.php file in any test file that needs to run in HHVM
-// and actually pass the Hack type checker since *strict* Hack
-// files cannot have top-level statements other than "require"s.
-main();
